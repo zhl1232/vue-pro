ae3b5b05d9c7eb817141b6d75f8e7e99585695ae
arron <66020423@qq.com>
2020-03-07 20:01:43 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Hom111e</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<style lang="less">
+
+</style>
